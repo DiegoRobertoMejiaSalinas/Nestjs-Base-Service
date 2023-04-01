68a9e96c2a2c3df6a1e2461dca7aecf960f41078
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-04-01 18:44:41 -0500

    chore: Imported Database Module into App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
       isGlobal: true,
     }),
+    DatabaseModule.forRoot(),
   ],
 })
 export class AppModule {}
