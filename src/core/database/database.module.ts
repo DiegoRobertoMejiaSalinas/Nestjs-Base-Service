2340ef3fba56ed3503381077da59f5a9c6ecfde5
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-04-01 19:22:01 -0500

    chore: Modified import config service path

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,6 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configService } from './config/config.service';
+import { configService } from '../config/config.service';
 
 @Module({})
 export class DatabaseModule {
